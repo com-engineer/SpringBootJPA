65550df9903fe9c6ad45d404a40320b91fa91c30
com-engineer <gaurav0207vesit@gmail.com>
2026-08-29 15:25:55 +0530

    added some missed exception in the golbal exception handler of auth service

--- OrderManagementSystemMicroservices/auth-service/src/main/java/com/example/auth/exception/GlobalExceptionHandler.java
+++ OrderManagementSystemMicroservices/auth-service/src/main/java/com/example/auth/exception/GlobalExceptionHandler.java
@@ -1,11 +1,20 @@
 package com.example.auth.exception;
 
 import com.example.auth.dto.ErrorResponseDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.error.Error;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.security.GeneralSecurityException;
+import java.util.HashMap;
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -18,5 +27,50 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponseDto> handleUserNotFoundException(UserNotFoundException ex){
         return ResponseEntity.status(HttpStatus.CONFLICT).body(new ErrorResponseDto("USER_NOT_FOUND",ex.getMessage()));
     }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ErrorResponseDto> handleAccessDeniedException(AccessDeniedException ignored) {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                .body(new ErrorResponseDto("ACCESS_DENIED", "You are not allowed to perform this action"));
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ErrorResponseDto> handleAuthenticationException(AuthenticationException ignored) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body(new ErrorResponseDto("AUTH_FAILED", "Invalid email or password"));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponseDto> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex){
+
+        HashMap<String,String> fieldErrors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(
+                error -> fieldErrors.put(error.getField(), error.getDefaultMessage())
+        );
+
+        StringBuilder errorMsg = new StringBuilder();
+        boolean isFirst = true;
+
+        for(String field : fieldErrors.keySet()){
+            if(!isFirst){
+                errorMsg.append(", ");
+            }
+            isFirst = false;
+            errorMsg.append(field).append(" : ").append(fieldErrors.get(field));
+
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).
+                body(new ErrorResponseDto("INVALID_INPUT",errorMsg.toString()));
+    }
+
+    @ExceptionHandler(GeneralSecurityException.class)
+    public ResponseEntity<ErrorResponseDto> handleGenericException(Exception ex){
+        log.error("An unexpected error occurred");
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ErrorResponseDto("INTERNAL_ERROR", "An unexpected error occurred"));
+    }
+
+
+
 
 }

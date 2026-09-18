188b9bb8e628356ecc0705ac6fd0c30530bb0cac
com-engineer <gaurav0207vesit@gmail.com>
2026-09-18 19:35:43 +0530

    created new project to practice firebase services

---
+++ firebase-notes-api/src/test/java/com/example/firebase_notes_api/FirebaseNotesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firebase_notes_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirebaseNotesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
